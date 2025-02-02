abdbc51a33566cb15c3ac408788ed5c2cdd6d8c1
Aditya Shrish Puranik <41595533+adityapuranik99@users.noreply.github.com>
2025-02-01 19:59:48 -0800

    sync: add solution for

---
+++ solutions/1-two-sum.py
@@ -0,0 +1,4 @@
+//  - LeetCode 1
+// Link: https://leetcode.com/problems/two-sum/
+
+12345678class Solution:    def twoSum(self, nums: List[int], target: int) -> List[int]:        store_array = {}        for i in range(len(nums)):            if target - nums[i] in store_array:                return [store_array[target-nums[i]], i]            store_array[nums[i]] = i        return []
